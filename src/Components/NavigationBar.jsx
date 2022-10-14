3fc2aacf434006713a12409ca774a4805c780f70
sjodave <sjodave@gmail.com>
2022-10-14 19:04:42 +0530

    c3

--- src/Components/NavigationBar.jsx
+++ src/Components/NavigationBar.jsx
@@ -12,7 +12,7 @@ function NavScrollExample() {
         <Navbar.Collapse id="navbarScroll">
           <Nav
             className="me-auto my-2 my-lg-0"
-            style={{ maxHeight: "100px" }}
+            style={{ maxHeight: "60vh" }}
             navbarScroll
           >
             <Nav.Link href="/">Home</Nav.Link>
