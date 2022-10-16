cbfb7abec352fd989cf1017829df834fe343fbaa
sjodave <sjodave@gmail.com>
2022-10-16 05:55:57 +0530

    c5

--- src/Components/Events/Events.jsx
+++ src/Components/Events/Events.jsx
@@ -3,12 +3,12 @@ import { event } from "../ImageArray";
 export default function Events() {
   return (
     <div className="container">
-      <h1>Events</h1>
+      <h1 className="dark dFlex-center">Events</h1>
       <div className="row dFlex-space-around">
         {event.map((e) => {
           return (
             <>
-              <p className="col-sm-12 col-lg-6">
+              <p className="col-sm-12 col-lg-6 card p-3">
                 <b>{e.name} - </b>
                 {e.info}
               </p>
